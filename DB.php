334c9011dfb8a2806c795cecc1b4da009bb09ce6
BlessedSamhain <wioletta.wysopal@gmail.com>
2023-05-29 16:14:08 +0200

    PDO pgsql

---
+++ DB.php
@@ -0,0 +1,23 @@
+<?php
+// PostgreSQL database credentials
+    class DB{
+        private $con;
+        private $host = 'localhost';
+        private $port = '5432';
+        private $dbname = 'mydatabase';
+        private $user = 'postgres';
+        private $password = 'password';
+
+        //using for creating connection between database
+        public function __construct(){
+            //database source name - dsn
+            $dsn = "pgsql:host=".$this->host.";dbname=".$this->dbname;
+            try {
+                $this->con = new PDO($dsn, $this->user, $this->password);
+                $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "connection successfull";
+            } catch(PDOException $e) {
+                echo "Connection failed: ".$e->getMessage();
+            }
+    }
+}
